f7f1c7b566465e9890b72ee7ee6e94e0405e4088
Shafqat Sarwar <khansarwar1@hotmail.com>
2025-12-25 23:32:11 +0000

    .

--- config_fix.js
+++ config_fix.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Next.js 15+ ignores
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-
+    typescript: { ignoreBuildErrors: true },
+    eslint: { ignoreDuringBuilds: true }
 };
-
 module.exports = nextConfig;
